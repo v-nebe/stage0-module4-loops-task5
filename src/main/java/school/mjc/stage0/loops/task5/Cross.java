3fdfac69c0d86d66d9fc9455fe273ed39680d6dc
v-nebe <katya.shauneva@gmail.com>
2023-03-05 20:40:54 +0300

    Tasks completed

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
+        for (int i = 1; i<=sideLength; i++){
+            for (int j=1; j<=sideLength; j++) {
+                if (j == Math.round(sideLength/2.0) || i == Math.round(sideLength/2.0))
+                    System.out.print(8);
+                else
+                    System.out.print(" ");
+            }
+            System.out.println("");
+
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        for (int i = 0; i<height; i++){
+            for (int j=0; j<height; j++) {
+                if (i <= j && i < height - j || i >= j && i >= height - j - 1) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
     }
 }
